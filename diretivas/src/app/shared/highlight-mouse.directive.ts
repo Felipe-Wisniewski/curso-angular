8c3370ecd2e4ac076f553042e11fdf5821f6be36
Felipe Wisniewski <felipe.gomes@deliverit.com.br>
2019-04-05 17:16:18 -0300

    aula #39

---
+++ diretivas/src/app/shared/highlight-mouse.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, HostListener, ElementRef, Renderer2, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[highlightMouse]'
+})
+export class HighlightMouseDirective {
+
+  @HostListener('mouseenter') onMouseEnter() {
+    // this._renderer.setStyle(this._elementRef.nativeElement, 'background-color', 'yellow');
+    this.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    // this._renderer.setStyle(this._elementRef.nativeElement, 'background-color', 'white');
+    this.backgroundColor = 'white';
+  }
+
+  // @HostBinding('style.backgroundColor') backgroundColor: string;
+  //se precisar manipular algo durante o evento
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+  
+  private backgroundColor: string;
+
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer2) { }
+
+}
