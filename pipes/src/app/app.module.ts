5f0ebb8b2cda5498caddfdf0a7cfbdd23894e2dc
Felipe Wisniewski <felipewisniewski@gmail.com>
2019-04-08 18:46:20 -0300

    merge

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -3,25 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { CursosModule } from './cursos/cursos.module';
-import { CriarCursoModule } from './criar-curso/criar-curso.module';
-import { LogService } from './shared/log.service';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemplosPipesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CriarCursoModule,
-    CursosModule
+    AppRoutingModule
   ],
-  providers: [
-    // CursosService,
-    LogService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { LogService } from './../shared/log.service';
 
 @Injectable()
 export class CursosService {
 
-    getCursos() {
-        return  ['Angular', 'React', 'Flutter'];
-    }
+  emitirCursoCriado = new EventEmitter<string>();
+
+  private cursos: string[] = ['Angular', 'React', 'Flutter'];
+
+  constructor(private logService: LogService) {
+    console.log('CursosServices');
+  }
+
+  getCursos() {
+    this.logService.consoleLog('obtendo cursos');
+    return  this.cursos;
+  }
+
+  addCurso(curso: string) {
+    this.logService.consoleLog(`adicionando um novo curso: ${curso}`);
+    this.cursos.push(curso);
+    this.emitirCursoCriado.emit(curso);
+  }
 
 }
-//todo serviço com @Injectable tem que ser declarado no providers:[] do modulo app.module
+//todo serviço com @Injectable tem que ser declarado no providers:[] do modulo local ou global(app.module)
