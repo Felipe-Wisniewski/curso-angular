658c98098e082c035ed6d20301523eaae9967037
Felipe Wisniewski <felipewisniewski@gmail.com>
2019-04-21 20:29:27 -0300

    #aula 124

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -2,18 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 import { CursosService } from '../cursos.service';
 import { Curso } from './../curso';
-import { Observable } from 'rxjs';
+import { Observable, empty, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
   // cursos: Curso[];
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
@@ -21,7 +24,14 @@ export class CursosListaComponent implements OnInit {
     /* this.service.list().subscribe(
       dados => this.cursos = dados
     ); */
-    this.cursos$ = this.service.list();
+    this.cursos$ = this.service.list()
+      .pipe(
+        catchError(error => {
+          console.error(error);
+          this.error$.next(true);
+          return empty();
+        })
+      );
   }
 
 }
--- requests-http/src/app/cursos/cursos.service.ts
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { tap, delay } from 'rxjs/operators'
 import { Curso } from './curso';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   constructor(private http: HttpClient) { }
 
